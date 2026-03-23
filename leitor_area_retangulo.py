c0d4bd08eabe83c0f0344dea01b2954542b3908d
josiascardosodias-glitch <josiascardosodias@gmail.com>
2026-03-23 20:48:55 -0300

    correção na aula

--- leitor_area_retangulo.py
+++ leitor_area_retangulo.py
@@ -5,8 +5,8 @@ from a_uteis_Dev import uteis
 
 uteis.cabecalho("Insira o valor da base e da altura de um retângulo para descobrir a sua área")
 
-base = float(input("Digite o valor da base do nosso retângulo: "))
-altura = float(input("Digite o valor da altura do nosso retângulo: "))
+base = float(input("Digite o valor da base do nosso retângulo: ")) # Entrada de dados
+altura = float(input("Digite o valor da altura do nosso retângulo: ")) # Entrada de dados
 area = base * altura
 
-print(f"A área do nosso triangulo é de {area:.1f} Metros quadrados")
+print(f"A área do nosso retângulo é de {area:.2f} Metros quadrados") # Saída de dados
--- raizes_bhaskara.py
+++ raizes_bhaskara.py
@@ -2,13 +2,13 @@
 equação de báscara. """
 
 from a_uteis_Dev import uteis
-from math import sqrt
+import math
 
 uteis.cabecalho("Vamos calcular o valor das raizes de uma formula de bhaskara")
 a = int(input("Digite o valor de a: "))
 b = int(input("Digite o valor de b: "))
-delta = int(input("Digite o valor de delta: "))
-raiz1 = (-b + sqrt(delta)) / (2 * a)
-raiz2 = (-b - sqrt(delta)) / (2 * a)
+c = int(input("Digite o valor de c: "))
+raiz1 = (-b + math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+raiz2 = (-b - math.sqrt(b**2 - 4 * a * c)) / (2 * a)
 
-print(f"O valor da primeira raiz é {raiz1} e o valor da segunda raiz é {raiz2}")
+print(f"O valor da primeira raiz é {raiz1:.2f} \nE o valor da segunda raiz é {raiz2:.2f}")
