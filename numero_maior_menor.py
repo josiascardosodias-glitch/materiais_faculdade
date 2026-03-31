f621ba6fa9a611f3ad1f65619621f6f953247c0f
josiascardosodias-glitch <josiascardosodias@gmail.com>
2026-03-30 22:10:46 -0300

    correção de exercicio em sala

---
+++ numero_maior_menor.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome: ")
+v1 = int(input("Digite um valor: "))
+v2 = int(input("Digite um outro valor: "))
+if v1 > v2:
+    print(f"O número {v1} é maior que o número {v2}")
+if v1 < v2:
+    print(f"O número {v2} é maior que o número {v1}")
+if v1 == v2:
+    print(f"O número {v1} e o número {v2} posuem o mesmo valor")
