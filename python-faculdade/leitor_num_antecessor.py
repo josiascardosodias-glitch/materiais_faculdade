5da88b71cd9cbe0363124d06fabb4a33dab9c0d6
josiascardosodias-glitch <josiascardosodias@gmail.com>
2026-04-15 15:25:49 -0300

    new main

--- python-faculdade/leitor_num_antecessor.py
+++
@@ -1,8 +0,0 @@
-""" Escreva um programa para ler um valor (do teclado) e escrever (na tela) o
-seu antecessor. """
-
-from a_uteis_Dev import uteis
-
-uteis.cabecalho("Digite um número e veja o seu antecessor")
-num = int(input("Digite um número: "))
-print(f"Você digitou o número {num}, seu antecessor é o número {num-1}")
